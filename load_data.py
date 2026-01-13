991cc2d49c07620d4a3e3114baa6c578d7ebe13a
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2026-01-13 11:41:05 +0900

    Add data: 민법.txt, 헌법.txt, 형법.txt and load_data.py script

---
+++ load_data.py
@@ -0,0 +1,14 @@
+import os
+
+pwd = "./data/"
+t = os.listdir(pwd)
+DATA = dict()
+for i in t:
+    f = open(os.path.join(pwd, i), "rt")
+    r = f.readlines()
+    f.close()
+    r = [x.strip() for x in r]
+    r = [x for x in r if x]
+    DATA[i] = r
+if __name__ == "__main__":
+    print(DATA)
